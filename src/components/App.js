9b048681f7b058f2b1d4311bf883c17a2cc4b58f
girishyadav <girishayadav.com>
2024-08-13 12:51:35 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setTime(prevSeconds => prevSeconds + 1);
+    }, 1000)
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
   return (
     <div>
         {/* Do not remove the main div */}
+        You've been on this page for {time} seconds.
     </div>
   )
 }
